0c1a9388504f3f5f0c7a27c216cbb0570145c0a5
RamazanBuhara <buhara.rmzn@gmail.com>
2022-12-21 21:06:33 +0300

    v2

---
+++ src/deneme2.java
@@ -0,0 +1,10 @@
+public class deneme2 {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("deneme222");
+
+    }
+}
--- src/githhubdeneme.java
+++ src/githhubdeneme.java
@@ -3,5 +3,7 @@ public class githhubdeneme {
     public static void main(String[] args) {
 
         System.out.println("Hello gitHub");
+
+        System.out.println("github deneme1");
     }
 }
